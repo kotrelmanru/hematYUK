8c9c5165dae5c00436f014226b387002843fa89e
Kotrel Manurung <kotrel@MacBook-Pro-Kotrel.local>
2025-05-26 02:19:35 +0700

    fix deploy 17

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,5 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 //Serve upload folder
 app.use("/uploads", express.static(path.join(__dirname,"uploads")));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
